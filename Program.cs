eed4f92dcfd464b13ac1f8758caad5aca073ac77
meewrage <luki_wop@mail.ru>
2023-02-12 03:32:22 +0300

    added task1

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+Console.Write("max = {0} min = {1}", a, b);
+else Console.Write("max = {0} min = {1}", b, a);
