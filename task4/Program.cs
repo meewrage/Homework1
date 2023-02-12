1a799b865938cb859e6172cc44653b2856e501b7
meewrage <luki_wop@mail.ru>
2023-02-12 03:52:03 +0300

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите три числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+max = a;
+if (a > b)
+{
+    max = a;
+}
+else (max = b);
+if (b > c)
+{
+    Console.Write(max);
+}
+else (max = c);
+Console.Write(max);
